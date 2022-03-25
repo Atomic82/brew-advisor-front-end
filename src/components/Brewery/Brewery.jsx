7d01d7ed7ff13a543bd44702d44547b1cc737745
RohanPNaher <RohanPNaher@gmail.com>
2022-03-25 16:06:37 -0400

    Add api information to ui

--- src/components/Brewery/Brewery.jsx
+++ src/components/Brewery/Brewery.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 
-const Brewery = () => {
+const Brewery = ({brewery}) => {
   return (
     <>
-    <Link to="/brewery">
-      <h1>Howdy, I am a specific brewery</h1>
+    <Link 
+      to="/brewery"
+      state={{brewery}}
+      className='brewery-link'
+    >
+      <p>
+        Go to {brewery.name}
+      </p>
     </Link>
     </>
   );
