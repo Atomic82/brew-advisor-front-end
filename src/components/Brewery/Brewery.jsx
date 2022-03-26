b655a5cb02247d9ecf301a2ec6219b3235430604
RohanPNaher <RohanPNaher@gmail.com>
2022-03-26 18:04:04 -0400

    add loading placeholder to breweries page and images to brewery component

--- src/components/Brewery/Brewery.jsx
+++ src/components/Brewery/Brewery.jsx
@@ -1,19 +1,22 @@
 import { Link } from "react-router-dom";
 
-const Brewery = ({brewery}) => {
+const Brewery = ({ brewery }) => {
   return (
     <>
-    <Link 
-      to="/brewery"
-      state={{brewery}}
-      className='brewery-link'
-    >
-      <p>
-        Go to {brewery.name}
-      </p>
-    </Link>
+      <Link
+        to="/brewery"
+        state={{ brewery }}
+        className='brewery-link'
+      >
+        <div>
+          <img src={brewery.image_url} alt="" style={{"max-height":"400px",}}/>
+          <p>
+            Go to {brewery.name}
+          </p>
+        </div>
+      </Link>
     </>
   );
 }
- 
+
 export default Brewery;
--- src/pages/BreweryList/BreweryList.jsx
+++ src/pages/BreweryList/BreweryList.jsx
@@ -16,10 +16,16 @@ const BreweryList = () => {
   return (
     <>
       <h1>I am the BreweryList</h1>
-      <div>
-        {breweries.map(brewery => (
-          <Brewery key={brewery.id} brewery={brewery} />
-        ))}
+      <div className='breweries-main'>
+        {breweries.length ? 
+          <div className='brewery-container'>
+            {breweries.map(brewery => (
+              <Brewery key={brewery.id} brewery={brewery} />
+            ))}
+          </div>
+          :
+          <img src="https://i.imgur.com/TTFdKY0.png" alt="logo-loading" className='loading' />
+        }
       </div>
     </>
   );
