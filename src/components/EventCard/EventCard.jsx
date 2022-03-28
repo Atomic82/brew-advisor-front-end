e445e6492e14a559d021fb5e8eb11544dda208d7
RohanPNaher <RohanPNaher@gmail.com>
2022-03-28 11:16:03 -0400

    add event detail view

--- src/components/EventCard/EventCard.jsx
+++ src/components/EventCard/EventCard.jsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 
-const Event = () => {
+const Event = ({ event }) => {
   return (
     <>
-    <Link to="/event">
-      <h1>Howdy, I am a specific event</h1>
+    <Link 
+      to={"/event/" + event._id}
+      state={{ event }}
+      className='event-link'
+    >
+      <div>
+        <p>View {event.name}</p>
+      </div>
     </Link>
     </>
   );
