ebb19b32e272d820e39505c604f15236cfebee48
RohanPNaher <RohanPNaher@gmail.com>
2022-03-31 13:07:11 -0400

    edit how event details are loaded

--- src/pages/EventDetails/EventDetails.jsx
+++ src/pages/EventDetails/EventDetails.jsx
@@ -6,53 +6,56 @@ import EventComment from "../../components/EventComment/EventComment";
 const EventDetails = ({ handleDeleteEvent, user }) => {
   const location = useLocation()
   const event = location.state.event
-  // const searchId = { id: event.brewery }
-
-  // const [eventBrewery, setEventBrewery] = useState([])
-
-  // useEffect(() => {
-  //   if (user) {
-  //     breweryService.getOneBreweryById(searchId)
-  //       .then(brewery => {
-  //         setEventBrewery(brewery)
-  //         console.log(brewery)
-  //       })
-  //   }
-  // }, [user])
+  const brewery = event.brewery
+  let date = new Date(event.timeDate).toLocaleString()
 
   return (
     <>
-      
-        <>
-          <h1>{event.name}</h1>
-          <img
-            src={event.brewery.photos[1]}
-            alt={event.brewery.name}
-          />
-          <h2>This event is being held at {event.brewery.name} and is hosted by {event.owner[0].name}</h2>
-          <h2>this event will be occuring on {event.timeDate}</h2>
-          <h3>{event.description}</h3>
-          {user.profile === event.owner[0]?._id ?
-            <div className="edit-delete-container">
-              <Link
-                to='/edit'
-                state={{ event }}
-              >
-                Edit the event
-              </Link>
-              <button
-                onClick={() => handleDeleteEvent(event._id)}
-              >
-                Delete event
-              </button>
+
+      <>
+        <h1>{event.name}</h1>
+        <img
+          src={brewery.photos[1] ? brewery.photos[1] : brewery.photos[0]}
+          alt={brewery.name}
+        />
+        <h2>{event.owner[0].name} is hosting an event at {brewery.name} on {date}</h2>
+        {/* <h2>this event will be occuring on {date}</h2> */}
+        <h3>{event.description}</h3>
+
+        <div className='event-venue-details'>
+          <div className='event-venue-address'>
+            <h3>Venue Address:</h3>
+            <p>{brewery.location.display_address[0]}</p>
+            <p>{brewery.location.display_address[1]}</p>
+          </div>
+          {brewery.phone !== '' &&
+            <div className='event-venue-phone'>
+              <p>{brewery.display_phone}</p>
             </div>
-            :
-            <div>hah no edits or delete</div>
           }
-          <Link to='/events'>Go back to All Events</Link>
-          <EventComment />
-        </>
-      
+          <div className='event-venue-url'>
+            <a href={brewery.url} target="_blank" rel="noreferrer">Check out the venue on Yelp</a>
+          </div>
+        </div>
+        {(user.profile === event.owner[0]?._id) &&
+          <div className="edit-delete-container">
+            <Link
+              to='/edit'
+              state={{ event }}
+            >
+              Edit the event
+            </Link>
+            <button
+              onClick={() => handleDeleteEvent(event._id)}
+            >
+              Delete event
+            </button>
+          </div>
+        }
+        <Link to='/events'>Go back to All Events</Link>
+        <EventComment />
+      </>
+
     </>
   );
 }
